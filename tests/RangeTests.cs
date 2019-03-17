8c3a4c0d8395ea7829abc6c9c4b217f5676c7714
adrian <cyberboy.havana@gmail.com>
2019-03-17 10:31:30 +0100

    Added new failed test

---
+++ tests/RangeTests.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Linq;
+using api.Controllers;
+using Xunit;
+
+namespace tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var range = new Range { Count = 3 };
+            var generated = range.Of(() => string.Empty);
+
+            Assert.Equal(3, generated.Count());
+        }
+        
+        [Fact]
+        public void SortShouldOrderResults()
+        {
+            var range = new Range { Count = 3, Sort = true };
+            var values = new[] { "a", "c", "b" };
+            var counter = 0;
+            var generated = range.Of(() => values[counter++]);
+
+            Assert.Equal(new[] { "a", "b", "c" }, generated.ToArray());
+        }
+    }
+}
